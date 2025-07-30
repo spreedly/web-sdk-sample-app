ee9df35def0cf27e5cdff642df677dfca9d0b6d7
omkar-joshi <ojoshi@spreedly.com>
2025-07-30 14:16:20 +0530

    Added cors config

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,12 @@ import authRoutes from './routes';
 import { errorHandler } from './middlewares/errorHandler';
 import path from 'path';
 import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
 
 app.use(morgan('combined'));
+app.use(cors());
 
 app.use('/', express.static(path.join(__dirname, 'static')));
 
