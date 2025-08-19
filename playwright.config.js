483de02274cb6512c526dfb70b558abb06895ff4
aniketspreedly <achanana@spreedly.com>
2025-08-19 18:44:04 +0530

    Added playwright config for BS

---
+++ playwright.config.js
@@ -0,0 +1,23 @@
+// This is a sample config for what users might be running locally
+const config = {
+  testDir: "./tests",
+  testMatch: "**/bstack_test*.js",
+
+  timeout: 90 * 1000,
+  expect: {
+    timeout: 5000,
+  },
+  workers: 1,
+  reporter: "line",
+  projects: [
+    {
+      name: "chrome",
+      use: {
+        browserName: "chromium",
+        channel: "chrome",
+      },
+    },
+  ],
+};
+
+module.exports = config;
