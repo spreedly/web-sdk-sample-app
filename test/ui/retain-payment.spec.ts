13ecd831cbfa4793e93c38006ee4c5a05a20440d
SMohrir <smohrir@spreedly.com>
2025-12-30 13:13:12 +0530

    Embedded mode recaching and changes to bin name in .json file (#45)
    * Embedded mode recaching and changes to bin name in .json file
    
    * Embedded mode recaching

--- test/ui/retain-payment.spec.ts
+++ test/ui/retain-payment.spec.ts
@@ -85,6 +85,7 @@ test.describe('Recaching Option', () => {
         await expect(apiResponse.transaction.payment_method.storage_state).toBe('retained');
         expect([200, 204]).toContain(responseStatus);
     })
+
     test('Use saved payment method in express checkout', async({page}) => {
         await page.goto(URLS.BASE);
         await waitForAuthParams(page);
@@ -146,5 +147,70 @@ test.describe('Recaching Option', () => {
         await expect(newMessageText).toMatch(/CVV Recached/);
     })
 
+    test('use saved payment method in embedded mode express checkout', async({page}) => {
+      await page.goto(URLS.BASE);
+      await waitForAuthParams(page);
+      const openEmbeddedModeCheckbox = page.getByTestId(SELECTORS.OPEN_IN_EMBEDDED_MODE);
+      await expect(openEmbeddedModeCheckbox).toBeVisible();
+      await openEmbeddedModeCheckbox.check();
+      await expect(openEmbeddedModeCheckbox).toBeChecked();
+      const iframe = page.frameLocator(SELECTORS.EXPRESS_IFRAME);
+      const expressButton = page.getByTestId(SELECTORS.EXPRESS_BUTTON);
+      await expect(expressButton).toBeEnabled();
+      await expressButton.click();
+      const payButton = await fillExpressCheckoutForm(page);
+      const closeButton = iframe.locator(SELECTORS.CLOSE_PAYMENT_FORM);
+      await payButton.click();
+      const tokenMessage = page.locator(SELECTORS.TOKEN_CONTAINER_MESSAGE);
+      await tokenMessage.waitFor({ state: 'visible', timeout: 30000 });
+      await page.waitForTimeout(1000);
+      const messageText = await tokenMessage.textContent();
+      console.log('Message Text:', messageText);
+      await expect(messageText).toMatch(/Token/);
+      await expect(messageText).toMatch(/Retained/);
+      const saveCardContent = page.locator(SELECTORS.SAVE_CARD_CONTENT);
+      const useCardButton = page.locator(SELECTORS.USE_CARD_BUTTON);
+      await expect(saveCardContent).toBeVisible();
+      await saveCardContent.click();
+      await expect(page.locator(SELECTORS.SAVED_CARD_CHECKMARK)).toBeVisible();
+      await expect(useCardButton).toBeEnabled();
+      await useCardButton.click();
+      const newIframe = page.frameLocator(SELECTORS.RECACHE_IFRAME);
+      const formTitle = newIframe.locator(SELECTORS.RECACHE_TILE);
+      await expect(formTitle).toHaveText(HEADINGS.EXPRESS_TITLE);
+      const cvvField = newIframe.locator(`input[placeholder="${PLACEHOLDERS.RECACHE_CVV}"]`);
+      await expect(cvvField).toBeVisible();
+      await cvvField.fill(TEST_DATA.CVV);
+      await expect(cvvField).toHaveValue(TEST_DATA.CVV);
+      let responseStatus: number | null = null;
+      let apiResponse: any = null;
+      page.on('response', async (response) => {
+          const url = response.url();
+          if (url.includes('recache.json')){
+            responseStatus = response.status();
+            console.log('Recache API Response Status:', responseStatus);
+            try {
+              apiResponse = await response.json();
+              console.log('Retain API Request URL:', url);
+              console.log('Retain API Response Status:', responseStatus);
+            } catch (error) {
+              console.log('Response is not JSON or empty');
+            }
+          }
+        });
+      const retainpayButton = newIframe.getByTestId(SELECTORS.EXPRESS_SUBMIT_BUTTON);
+      await retainpayButton.click();
+      await expect(tokenMessage).not.toContainText('Retained', { timeout: 10000 });
+      const newTokenMessage = page.locator(SELECTORS.TOKEN_CONTAINER_MESSAGE);
+      await newTokenMessage.waitFor({ state: 'visible', timeout: 5000 });
+      expect([200, 204]).toContain(responseStatus);
+      await expect(page.locator(SELECTORS.TOKEN_CONTAINER_MESSAGE)).toBeVisible();
+      const newMessageText = await newTokenMessage.textContent();
+      console.log('New Message Text:', newMessageText);
+      await expect(newMessageText).toMatch(/Token/);
+      await expect(newMessageText).toMatch(/CVV Recached/);
+    })
+
 })
+
 
