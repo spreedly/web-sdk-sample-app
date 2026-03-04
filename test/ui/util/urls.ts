9d1b926d1c5a23cbb43f232768eec31ab002391f
sourabh mohrir <smohrir@spreedly.com>
2026-03-04 23:25:43 +0530

    Url change

--- test/ui/util/urls.ts
+++ test/ui/util/urls.ts
@@ -1,9 +1,9 @@
 export const MONOREPO_URLS = {
-    BASE: "/monorepo",
-    TOKENIZE: "/monorepo/tokenize/index.html",
-    RECACHE: "/monorepo/recache/index.html",
-    PURCHASE: "/monorepo/purchase/index.html",
-    PURCHASE_3DS: "/monorepo/purchase-with-3ds/index.html",
+    BASE: "/",
+    TOKENIZE: "/tokenize/index.html",
+    RECACHE: "/recache/index.html",
+    PURCHASE: "/purchase/index.html",
+    PURCHASE_3DS: "/purchase-with-3ds/index.html",
     Payment_Methods: "/v1/payment_methods",
   } as const;
 
