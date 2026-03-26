9b62665aeab6276cd5d931c5a4b96a6bb28cf777
sourabh mohrir <smohrir@spreedly.com>
2026-03-26 11:24:21 +0530

    Cleaup of old framework

--- test/ui/testCases/card-number-cvv-validations.spec.ts
+++ test/ui/testCases/card-number-cvv-validations.spec.ts
@@ -35,32 +35,49 @@ test.describe('Card Number and CVV Validation', () => {
   });
 
   test('shows validation for invalid card in hosted fields', async ({ page }) => {
-      
-
-    })
-
-  test('shows validation for too-short card number in express checkout', async ({ page }) => {
-    
-  });
-
-  test('Shows validation for too short card number in hosted fields', async ({ page }) => {
-    
+    await page.goto(MONOREPO_URLS.BASE);
+    await landingPage.clickOnTokenizeButton(page);
+    await waitForAuthParams(page);
+    await tokenizePage.clickOnOpenPaymentFormButtonHostedFields(page);
+    await helperFunctions.fillHostedFieldsForm(page, TEST_DATA.INVALID_CARD_NUMBER, {
+      firstName: TEST_DATA.FIRST_NAME,
+      lastName: TEST_DATA.LAST_NAME,
+      cvv: TEST_DATA.CVV,
+      expiryMonth: TEST_DATA.EXPIRY_MONTH,
+      expiryYear: getValidYearString(),
+    });
+    await helperFunctions.verifyFormFieldsHostedFields(page, TEST_DATA.INVALID_CARD_NUMBER, {
+      cvv: TEST_DATA.CVV,
+      firstName: TEST_DATA.FIRST_NAME,
+      lastName: TEST_DATA.LAST_NAME,
+      expiryMonth: TEST_DATA.EXPIRY_MONTH,
+      expiryYear: getValidYearString(),
+    });
+    await expect(await helperFunctions.getHostedFieldsCardNumberField(page)).toHaveAttribute('aria-invalid', 'true');
+    await expect(page.frameLocator(SELECTORS.HOSTED_CVV_IFRAME).locator(`[aria-label="${ERROR_MESSAGES.INVALID_CARD_NUMBER}"]`).first()).toBeVisible()
   });
 
   test('CVV validation for express checkout', async ({ page }) => {
-   
+    await page.goto(MONOREPO_URLS.BASE);
+    await landingPage.clickOnExpressCheckoutButton(page);
+    await landingPage.clickOnTokenizeButton(page);
+    await waitForAuthParams(page);
+    await tokenizePage.clickOnOpenPaymentFormButtonExpressCheckout(page);
+    await helperFunctions.fillExpressCheckoutForm(page, TEST_DATA.INVALID_CARD_NUMBER, {
+      firstName: TEST_DATA.FIRST_NAME,
+      lastName: TEST_DATA.LAST_NAME,
+      cvv: TEST_DATA.INVALID_CVV_SHORT,
+      expiryMonth: TEST_DATA.EXPIRY_MONTH,
+      expiryYear: getValidYearString(),
+    });
+    await helperFunctions.verifyFormFieldsExpressCheckout(page, TEST_DATA.INVALID_CARD_NUMBER, {
+      cvv: TEST_DATA.INVALID_CVV_SHORT,
+      firstName: TEST_DATA.FIRST_NAME,
+      lastName: TEST_DATA.LAST_NAME,
+      expiryMonth: TEST_DATA.EXPIRY_MONTH,
+      expiryYear: getValidYearString(),
+    });
+    await expect(await helperFunctions.getExpressCheckoutCvvField(page)).toHaveAttribute('aria-invalid', 'true');
+    await expect(page.frameLocator(SELECTORS.EXPRESS_IFRAME).locator(`[aria-label="${ERROR_MESSAGES.INVALID_CVV}"]`).first()).toBeVisible()
   });
-
-  test('CVV validation for hosted fields', async ({ page }) => {
-   
-  });
-
-  test('CVV validation for express checkout with AMEX', async ({ page }) => {
-    
-  });
-
-
-  test('CVV validation for hosted fields with AMEX', async ({ page }) => {
-   
-   }); 
 });
