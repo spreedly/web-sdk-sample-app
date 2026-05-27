545111be0ac29a8a4c62ae6d0e38ab2d0821213f
Rahul Agarwal <ragarwal@spreedly.com>
2026-05-27 23:22:32 +0530

    removed unintentonal code

--- src/static/tokenize/tokenize.js
+++ src/static/tokenize/tokenize.js
@@ -430,7 +430,6 @@ window.openExpressCheckoutForm = function () {
 
   sdk.on('ready', () => {
     isReady = true;
-    updateFormState()
     SpreedlyUtils.setButtonLoading('open-payment-form-btn', false);
     elements.expressCheckoutOpenSection().classList.add('hidden');
     elements.expressCheckoutForm().classList.remove('hidden');
